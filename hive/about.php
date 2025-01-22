21e87b7a5b06a83ac7818fdc4aa78d82de95e02b
Amal-Manoj-248 <amalmj248@gmail.com>
2025-01-22 19:37:21 +0530

    Add files via upload

---
+++ hive/about.php
@@ -0,0 +1,180 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+   
+	
+	<title>HiveConnect | About Us</title>
+	
+   
+	
+    <!--================================BOOTSTRAP STYLE SHEETS================================-->
+        
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	
+    <!--================================ Main STYLE SHEETs====================================-->
+	
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/menu.css">
+	<link rel="stylesheet" type="text/css" href="css/color/color.css">
+	<link rel="stylesheet" type="text/css" href="assets/testimonial/css/style.css" />
+	<link rel="stylesheet" type="text/css" href="assets/testimonial/css/elastislide.css" />
+	<link rel="stylesheet" type="text/css" href="css/responsive.css">
+
+	<!--================================FONTAWESOME==========================================-->
+		
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+        
+	<!--================================GOOGLE FONTS=========================================-->
+	
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
+		
+	<!--================================SLIDER REVOLUTION =========================================-->
+	
+	<link rel="stylesheet" type="text/css" href="assets/revolution_slider/css/revslider.css" media="screen" />
+		
+</head>
+<body>
+	<div class="preloader"><span class="preloader-gif"></span></div>
+	<div class="theme-wrap clearfix">
+		<!--================================responsive log and menu==========================================-->
+		<div class="wsmenucontent overlapblackbg"></div>
+		<div class="wsmenuexpandermain slideRight">
+			<a id="navToggle" class="animated-arrow slideLeft"><span></span></a>
+			<a href="#" class="smallogo"><img src="images/logo.png" width="120" alt="" /></a>
+		</div>
+		<!--================================HEADER==========================================-->
+		<?php include_once('includes/header.php');?>
+		
+		<!--================================PAGE TITLE==========================================-->
+		<div class="page-title-wrap bgorange-1 padding-top-30 padding-bottom-30"><!-- section title -->
+			<h4 class="white">About Us</h4>
+		</div><!-- section title end -->
+		<div class="sc-page"><!--sc-page-->
+			<!--================================PRICING PLAN SECTION 1==========================================-->
+		
+			<section class="pricing-section padding-top-70 padding-bottom-40">
+				<div class="container"><!-- section container -->
+					<div class="row pricing-wrap style-2 clearfix">
+						
+						<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+							<?php
+$sql="SELECT * from tblpage where PageType='aboutus'";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $row)
+{               ?>
+							<h6>About Us</h6>
+							<p>HiveConnect is a dynamic local service search engine built to bring communities closer together by connecting people
+								 with trusted, reliable services in their area. Whether you’re looking for a nearby plumber, a trusted electrician, or a popular café, 
+								HiveConnect provides a centralized hub where users can find, compare, and connect with local service providers quickly and easily.</p><br>
+								<h6>Mission</h6>
+								<p> To create a seamless bridge between local businesses and the communities they serve,
+									 empowering users to access trusted services and providers within their reach.</p><br>
+								<h6>Why Choose HiveConnect?</h6>
+								<p>HiveConnect isn’t just a directory; it’s a community-driven platform that encourages feedback and prioritizes user satisfaction. 
+									By focusing on local, trusted providers, HiveConnect ensures that users feel confident and secure when reaching out for help. 
+									Local businesses benefit from greater visibility, and users get the convenience of having essential services at their fingertips.</p><br>
+						</div><?php $cnt=$cnt+1;}} ?>
+						<div class="col-xs-12 col-sm-8 col-md-8 col-lg-8"><!-- .pricing -->
+							
+							<div class="pricing shadow-1 clearfix">
+								<img src="images/service.jpg" height="600" width="700" alt="" />
+							</div>
+						</div><!-- pricing end -->
+						
+					</div><!-- .row pricing end -->
+				</div><!-- container end -->
+<?php include_once('includes/footer.php');?>
+	</div>
+	
+	
+	<!--================================JQuery===========================================-->
+        
+	<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+	<script src="js/jquery.js"></script><!-- jquery 1.11.2 -->
+	<script src="js/jquery.easing.min.js"></script>
+	<script src="js/modernizr.custom.js"></script>
+	
+	<!--================================BOOTSTRAP===========================================-->
+        
+	<script src="bootstrap/js/bootstrap.min.js"></script>	
+	
+	<!--================================NAVIGATION===========================================-->
+	
+	<script type="text/javascript" src="js/menu.js"></script>
+	
+	<!--================================SLIDER REVOLUTION===========================================-->
+		
+	<script type="text/javascript" src="assets/revolution_slider/js/revolution-slider-tool.js"></script>
+	<script type="text/javascript" src="assets/revolution_slider/js/revolution-slider.js"></script>
+	
+	<!--================================MAP===========================================-->
+		
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBueyERw9S41n4lblw5fVPAc9UqpAiMgvM&amp;sensor=false"></script>
+	<script type="text/javascript" src="js/map.js"></script>
+	<!-- map with geo locations -->
+	
+	<script type="text/javascript" src="js/jquery.mapit.js"></script>
+	<script src="js/initializers.js"></script>
+	
+	<!--================================OWL CARESOUL=============================================-->
+		
+	<script src="js/owl.carousel.js"></script>
+    <script src="js/triger.js" type="text/javascript"></script>
+		
+	<!--================================FunFacts Counter===========================================-->
+		
+	<script src="js/jquery.countTo.js"></script>
+	
+	<!--================================jquery cycle2=============================================-->
+	
+	<script src="js/jquery.cycle2.min.js" type="text/javascript"></script>	
+	
+	<!--================================waypoint===========================================-->
+		
+	<script type="text/javascript" src="js/jquery.waypoints.min.js"></script><!-- Countdown JS FILE -->
+	
+	<!--================================RATINGS===========================================-->	
+	
+	<script src="js/jquery.raty-fa.js"></script>
+	<script src="js/rate.js"></script>
+	
+	<!--================================ testimonial ===========================================-->
+	<script id="img-wrapper-tmpl" type="text/x-jquery-tmpl">	
+			
+			<div class="rg-image-wrapper">
+				<div class="rg-image"></div>
+				<div class="rg-caption-wrapper">
+					<div class="rg-caption" style="display:none;">
+						<p></p>
+						<h5></h5>
+						<div class="caption-metas">
+							<p class="position"></p>
+							<p class="orgnization"></p>
+						</div>
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</script>	
+	<script type="text/javascript" src="assets/testimonial/js/jquery.tmpl.min.js"></script>
+	<script type="text/javascript" src="assets/testimonial/js/jquery.elastislide.js"></script>
+	<script type="text/javascript" src="assets/testimonial/js/gallery.js"></script>
+	
+	<!--================================custom script===========================================-->
+		
+	<script type="text/javascript" src="js/custom.js"></script>
+    
+</body>
+</html>
---
+++ hive/includes/footer.php
@@ -0,0 +1,14 @@
+<footer>
+            
+            <div class="footer-bottom">
+                <div class="container">
+                    <div class="row clearfix">
+                        <center>
+                        <p>&copy;Copyrights</p>
+                        <p>2024.All rights are reserved</p>
+                        <p>HiveConnect</p>
+                        </center>
+                    </div>
+                </div>
+            </div>
+        </footer>
